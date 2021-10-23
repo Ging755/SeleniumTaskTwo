30c0e063499a88fbfc5f1f49fb0b021529dc136d
Denis <denis.milic2@gmail.com>
2021-10-23 13:22:32 +0200

    added all dependecies needed and login page test completed

---
+++ SeleniumTaskTwo/SeleniumTaskTwo/PageObjects/LoginPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTaskTwo.PageObjects
+{
+    class LoginPage
+    {
+        private IWebDriver _driver;
+
+        public LoginPage()
+        {
+            _driver = MainWebDriver.Driver;
+        }
+
+        public IWebElement UserName => _driver.FindElement(By.Id("user-name"));
+        public IWebElement Password => _driver.FindElement(By.Id("password"));
+        public IWebElement LoginButton => _driver.FindElement(By.Id("login-button"));
+        public IWebElement ErrorMessage { get; set; }
+
+        public bool LoginPageDisplayed()
+        {
+            return (UserName.Displayed && Password.Displayed && LoginButton.Displayed) ;
+        }
+
+        public IWebElement Login(string username, string password)
+        {
+            UserName.SendKeys(username);
+            Password.SendKeys(password);
+            LoginButton.Submit();
+
+            try
+            {
+                return _driver.FindElement(By.XPath("//div[@class='error-message-container error']/h3"));
+            }
+            catch
+            {          
+                return _driver.FindElement(By.Id("inventory_container"));
+            }
+        }
+    }
+}
---
+++ SeleniumTaskTwo/SeleniumTaskTwo/Program.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using SeleniumTaskTwo.PageObjects;
+using System;
+
+namespace SeleniumTaskTwo
+{
+    class Program
+    {
+        [SetUp]
+        public void Initilize()
+        {
+            MainWebDriver.Driver = new ChromeDriver();
+
+            //Navigate to login Page
+            MainWebDriver.Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [Test]
+        public void LoginScreenDisplayed()
+        {
+            LoginPage loginPage = new LoginPage();
+            //Login page should be displayed along with username, password input and login button
+            Assert.IsTrue(loginPage.LoginPageDisplayed());
+        }
+
+        [Test]
+        public void IncorrectUserNameOrPassword()
+        {
+            LoginPage loginPage = new LoginPage();
+            //Checking if corret error message is displayed when username or password are incorrect
+            Assert.IsTrue(loginPage.Login("WrongUserName", "WrongPassword").Text.Equals("Epic sadface: Username and password do not match any user in this service"));
+        }
+
+        [Test]
+        public void EmptyUsername()
+        {
+            LoginPage loginPage = new LoginPage();
+            //Checking if corret error message is displayed when username is empty
+            Assert.IsTrue(loginPage.Login("", "WrongPassword").Text.Equals("Epic sadface: Username is required"));
+        }
+
+        [Test]
+        public void EmptyPassword()
+        {
+            LoginPage loginPage = new LoginPage();
+            //Checking if corret error message is displayed when password is empty
+            Assert.IsTrue(loginPage.Login("Username", "").Text.Equals("Epic sadface: Password is required"));
+        }
+
+        [Test]
+        public void SuccessfulLogin()
+        {
+            LoginPage loginPage = new LoginPage();
+            //After successfull login, products page should be displayed
+            Assert.IsTrue(loginPage.Login("standard_user", "secret_sauce").Displayed);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            MainWebDriver.Driver.Close();
+        }
+    }
+}
